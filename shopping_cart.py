d3db78cb167e3526e7ee4c2fd8a8b2f90c00d52c
sy592 <59641196+sy592@users.noreply.github.com>
2020-02-09 19:28:59 -0500

    Assemble Running Total Price

--- shopping_cart.py
+++ shopping_cart.py
@@ -28,6 +28,7 @@ products = [
 #INFORMATION INPUT
 #
 
+total_price = 0
 while True:
     selected_id = input("Please input a product identifier: ") #> "9" (string)
     #> "DONE"
@@ -36,11 +37,14 @@ while True:
     else:
         matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
         matching_product = matching_products[0]
+        total_price = total_price + matching_product["price"]
         print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
 
 #
 #INFORMATION OUPUT / DISPLAY
 #
+
+print("TOTAL PRICE: " + str(total_price))
 
 #TODO: write some code to output the required functionality
 #print(products)
