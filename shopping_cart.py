0f308f2a9a78d20016700b1ec8af4f6d888d6b5e
sy592 <59641196+sy592@users.noreply.github.com>
2020-02-09 19:34:58 -0500

    Seprate info input and output process

--- shopping_cart.py
+++ shopping_cart.py
@@ -29,20 +29,26 @@ products = [
 #
 
 total_price = 0
+selected_ids = []
 while True:
     selected_id = input("Please input a product identifier: ") #> "9" (string)
     #> "DONE"
     if selected_id == "DONE":
         break
     else:
+       selected_ids.append(selected_id)
+#
+#INFORMATION OUPUT / DISPLAY
+#
+
+#print(selected_ids)
+
+for selected_id in selected_ids:
         matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
         matching_product = matching_products[0]
         total_price = total_price + matching_product["price"]
         print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
-
-#
-#INFORMATION OUPUT / DISPLAY
-#
+        
 
 print("TOTAL PRICE: " + str(total_price))
 
